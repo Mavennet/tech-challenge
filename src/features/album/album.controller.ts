825a880ca8aff6fba0e40f52d94f9c476997b82b
Чудинов Денис <chudinovds@vtblife.ru>
2019-12-21 19:29:07 +0300

    hide before auth albums

--- src/features/album/album.controller.ts
+++ src/features/album/album.controller.ts
@@ -1,14 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards, UseInterceptors } from '@nestjs/common';
 import { AlbumService } from './album.service';
-import { Crud } from '@nestjsx/crud';
+import { Crud, CrudRequestInterceptor } from '@nestjsx/crud';
 import { Album } from './album.entity';
+import { AuthGuard } from '@nestjs/passport';
+import { UserInterceptor } from '../../infrastructure/user.interceptor';
 
 @Crud({
   model: {
     type: Album,
   },
+  query: {
+    join: {
+      user: {
+        persist: ['id'],
+        allow: ['id'],
+        eager: true,
+      },
+    },
+  },
 })
+@UseInterceptors(UserInterceptor, CrudRequestInterceptor)
 @Controller('albums')
+@UseGuards(AuthGuard('jwt'))
 export class AlbumController {
   constructor(public service: AlbumService) {}
 }
